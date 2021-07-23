ff9a3ff075f0df7c06bcf2874ab5c43a247c93d7
SherryWu178 <sherry.wu178@gmail.com>
2021-07-24 03:07:03 +0800

    try

--- jiobot.py
+++ jiobot.py
@@ -1,136 +1,65 @@
-# import logging
-# import telegram
-# from typing import Tuple, Optional
+from functions import *
 
-# from telegram import Update, Chat, ChatMember, ChatMemberUpdated
+import redis
+r = redis.Redis(host='localhost', port=6379)
 
-# from telegram.ext import (
-#         Updater,
-#         CommandHnadler,
-#         CallbackContext,
-#         ChateMemberHandler,
-#         )
-# bot = telegram.Bot(token='1892557011:AAFd_H0RKCLvZdIc9SHknIGH_ofi0q3W3Xc')
-# # Enable logging
-# logging.basicConfig(
-#         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-#         )
-
-# logger = logging.getLogger(__name__)
-
-# Set your bot_token here
-# (the same one as you've created and used just now!)
 import logging
 import requests
-import os
 
 from telegram.ext import MessageHandler, Filters
-from telegram.ext import CommandHandler, CallbackQueryHandler
-from telegram.ext import Updater
+from telegram.ext import CommandHandler
+from telegram.ext import Updater, CallbackQueryHandler, CallbackContext
 
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-
-bot_token = "1901886861:AAHA8pU8d6pbiBvbjiQm7d8dxCyIU1sMWU0"
-PORT = int(os.environ.get('PORT', 5000))
-
-# Importing python-telegram-bot's library functions
+bot_token = "1843778002:AAFxOoXX7xWTubiMmULqkTlQ9U0267zHa4I"
 
 # Setting up our logger
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
-
-
-def get_cat_fact():
-    endpoint = "https://catfact.ninja/fact"
-    response = requests.get(endpoint)
-    fact = response.json()["fact"]
-    return fact
-
-
-def get_dog_fact():
-    endpoint = "http://dog-api.kinduff.com/api/facts"
-    response = requests.get(endpoint)
-    fact = response.json()["facts"][0]
-    return fact
-
+                    level=logging.INFO)
 
 # Setting up their polling stuff
 updater = Updater(token=bot_token, use_context=True)
 dispatcher = updater.dispatcher
 
-# Functions to handle each command
+# Create and add command handlers
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+start_jio_handler = CommandHandler('start_jio', start_jio)
+dispatcher.add_handler(start_jio_handler)
+
+edit_jio_title_handler = CommandHandler('edit_jio_title', edit_jio_title)
+dispatcher.add_handler(edit_jio_title_handler)
+
+join_jio_handler = CommandHandler("join_jio", join_jio)
+dispatcher.add_handler(join_jio_handler)
+
+finalise_jio_handler = CommandHandler('finalise_jio', finalise_jio)
+dispatcher.add_handler(finalise_jio_handler)
+
+paid_handler = CommandHandler("paid", paid)
+dispatcher.add_handler(paid_handler)
+
+end_jio_handler = CommandHandler('end_jio', end_jio)
+dispatcher.add_handler(end_jio_handler)
+
+quit_jio_handler = CommandHandler('quit_jio', quit_jio)
+dispatcher.add_handler(quit_jio_handler)
+
+confirm_finalise_handler = CallbackQueryHandler(callback = confirm_finalise_jio, pattern = r'confirm_finalise' )
+dispatcher.add_handler(confirm_finalise_handler)
+
+cancel_handler = CallbackQueryHandler(callback = cancel, pattern = r'cancel' )
+dispatcher.add_handler(cancel_handler)
 
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Hi! To start a jio, type '/start_jio'")
+# Start the Bot
+updater.start_webhook(listen="0.0.0.0",
+                    port=int(PORT),
+                    url_path=bot_token)
 
-keyboard = [
-    [
-        InlineKeyboardButton("Button 1", switch_inline_query_current_chat='/add ', message_id= 12345),
-        InlineKeyboardButton("Button 2", switch_inline_query_current_chat='/remove ', message_id= 12345),
-    ]
-]
+updater.bot.setWebhook('https://floating-waters-24425.herokuapp.com/' + bot_token)
 
-reply_markup = InlineKeyboardMarkup(keyboard)
-
-def cat_asdf(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text=get_cat_fact(), reply_markup=reply_markup)
-
-
-def dog(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text=get_dog_fact())
-
-
-
-def echo(update, context):
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text=update.message.text)
-
-
-def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
-    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
-
-
-def main():
-    # Create and add command handlers
-    callback_handler = CallbackQueryHandler(callback = dog, pattern=r'\w*')
-    dispatcher.add_handler(callback_handler)
-
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
-
-    cat_handler = CommandHandler('cat', cat_asdf)
-    dispatcher.add_handler(cat_handler)
-
-    dog_handler = CommandHandler('dog', dog)
-    dispatcher.add_handler(dog_handler)
-
-    add_handler = CommandHandler('cat', cat_asdf)
-    dispatcher.add_handler(cat_handler)
-
-    remove_handler = CommandHandler('dog', dog)
-    dispatcher.add_handler(dog_handler)
-
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-    dispatcher.add_handler(echo_handler)
-
-    caps_handler = CommandHandler('caps', caps)
-    dispatcher.add_handler(caps_handler)
-
-    # updater.start_polling()
-
-    # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                        port=int(PORT),
-                        url_path=bot_token)
-
-    updater.bot.setWebhook('https://floating-waters-24425.herokuapp.com/' + bot_token)
-
-    updater.idle()
+updater.idle()
 
         
 
---
+++ util.py
@@ -0,0 +1,34 @@
+def parse(update, context):
+    arguments = context.args
+    if update.message:
+        user_id = update.message.from_user.id
+        user_name = update.message.from_user.name
+    else:
+        user_id = 0
+        user_name = ''
+
+    chat_id = update.effective_chat.id
+    return arguments, user_id, user_name, chat_id
+
+
+def stringify_ids(chat_id, user_id):
+    chat_id_meta_string = get_chat_id_meta_string(chat_id)
+    chat_id_item_string = get_chat_id_item_string(chat_id)
+    user_id_string = get_user_id_string(user_id)
+    return chat_id_meta_string, chat_id_item_string, user_id_string
+
+
+def get_chat_id_meta_string(chat_id):
+    return 'meta' + str(chat_id)
+
+def get_chat_id_item_string(chat_id):
+    return 'item' + str(chat_id)
+
+def get_user_id_string(user_id):
+    return 'u' + str(user_id)
+
+def get_open_jio_name_string(jio_name):
+    return '<b>' + '🍕 ' + jio_name + '</b>' + '\n\n'
+
+def get_finalised_jio_name_string(jio_name):
+    return '<b>' + '🍕 ' + jio_name + '</b> [finalised] \n' +"<i>Jio is finalised, indicate payment using '/paid'</i>\n\n"
