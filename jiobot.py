123998319b95402ee674bdd88068737cb9ee8723
SherryWu178 <sherry.wu178@gmail.com>
2021-07-24 03:37:35 +0800

    try

--- jiobot.py
+++ jiobot.py
@@ -21,50 +21,52 @@ PORT = int(os.environ.get('PORT', 5000))
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-# Setting up their polling stuff
-updater = Updater(token=bot_token, use_context=True)
-dispatcher = updater.dispatcher
 
-# Create and add command handlers
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+def main():
+    # Setting up their polling stuff
+    updater = Updater(token=bot_token, use_context=True)
+    dispatcher = updater.dispatcher
 
-start_jio_handler = CommandHandler('start_jio', start_jio)
-dispatcher.add_handler(start_jio_handler)
+    # Create and add command handlers
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
-edit_jio_title_handler = CommandHandler('edit_jio_title', edit_jio_title)
-dispatcher.add_handler(edit_jio_title_handler)
+    start_jio_handler = CommandHandler('start_jio', start_jio)
+    dispatcher.add_handler(start_jio_handler)
 
-join_jio_handler = CommandHandler("join_jio", join_jio)
-dispatcher.add_handler(join_jio_handler)
+    edit_jio_title_handler = CommandHandler('edit_jio_title', edit_jio_title)
+    dispatcher.add_handler(edit_jio_title_handler)
 
-finalise_jio_handler = CommandHandler('finalise_jio', finalise_jio)
-dispatcher.add_handler(finalise_jio_handler)
+    join_jio_handler = CommandHandler("join_jio", join_jio)
+    dispatcher.add_handler(join_jio_handler)
 
-paid_handler = CommandHandler("paid", paid)
-dispatcher.add_handler(paid_handler)
+    finalise_jio_handler = CommandHandler('finalise_jio', finalise_jio)
+    dispatcher.add_handler(finalise_jio_handler)
 
-end_jio_handler = CommandHandler('end_jio', end_jio)
-dispatcher.add_handler(end_jio_handler)
+    paid_handler = CommandHandler("paid", paid)
+    dispatcher.add_handler(paid_handler)
 
-quit_jio_handler = CommandHandler('quit_jio', quit_jio)
-dispatcher.add_handler(quit_jio_handler)
+    end_jio_handler = CommandHandler('end_jio', end_jio)
+    dispatcher.add_handler(end_jio_handler)
 
-confirm_finalise_handler = CallbackQueryHandler(callback = confirm_finalise_jio, pattern = r'confirm_finalise' )
-dispatcher.add_handler(confirm_finalise_handler)
+    quit_jio_handler = CommandHandler('quit_jio', quit_jio)
+    dispatcher.add_handler(quit_jio_handler)
 
-cancel_handler = CallbackQueryHandler(callback = cancel, pattern = r'cancel' )
-dispatcher.add_handler(cancel_handler)
+    confirm_finalise_handler = CallbackQueryHandler(callback = confirm_finalise_jio, pattern = r'confirm_finalise' )
+    dispatcher.add_handler(confirm_finalise_handler)
+
+    cancel_handler = CallbackQueryHandler(callback = cancel, pattern = r'cancel' )
+    dispatcher.add_handler(cancel_handler)
 
 
-# Start the Bot
-updater.start_webhook(listen="0.0.0.0",
-                    port=int(PORT),
-                    url_path=bot_token)
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                        port=int(PORT),
+                        url_path=bot_token)
 
-updater.bot.setWebhook('https://floating-waters-24425.herokuapp.com/' + bot_token)
+    updater.bot.setWebhook('https://floating-waters-24425.herokuapp.com/' + bot_token)
 
-updater.idle()
+    updater.idle()
 
         
 
