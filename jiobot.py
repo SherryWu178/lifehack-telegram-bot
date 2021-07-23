99ee19a8bbad0d2545e2f70f18da929e2d44b0fb
SherryWu178 <sherry.wu178@gmail.com>
2021-07-23 14:27:17 +0800

    change the token

--- jiobot.py
+++ jiobot.py
@@ -113,7 +113,7 @@ updater.start_polling()
 # Start the Bot
 updater.start_webhook(listen="0.0.0.0",
                       port=int(PORT),
-                      url_path=TOKEN)
+                      url_path=bot_token)
 updater.bot.setWebhook('https://floating-waters-24425.herokuapp.com/' + bot_token)
 
 updater.idle()
